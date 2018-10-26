2a9519fc51058010e264827fd4329e9e871edaf6
Adarsh Kumar <clydoskope@gmail.com>
2018-10-27 00:44:34 +0800

    adding basic chat backend

---
+++ public/js/chatbox.js
@@ -0,0 +1,15 @@
+var socket = io();
+socket.on('connect', () => {
+  console.log('connected to server');
+
+});
+
+socket.on('disconnect', () => {
+  console.log('disconnected to server');
+});
+
+socket.on('newMessage', (message) => {
+  console.log('New Message', message);
+});
+
+
