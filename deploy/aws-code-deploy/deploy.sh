c6b9d1047bba06afa096f5092244b51158bc5505
Adarsh Kumar <clydoskope@gmail.com>
2018-11-07 07:56:15 +0000

    Update deploy.sh

--- deploy/aws-code-deploy/deploy.sh
+++ deploy/aws-code-deploy/deploy.sh
@@ -4,9 +4,8 @@ apt-get --quiet install --yes nodejs
 apt-get  --quiet install --yes  npm
 apt-get  --quiet install --yes  python-pip # AWS CLI requires python-pip, python is installed by default
 python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall  # pip update 
-sudo pip install awscli --force-reinstall --upgrade  # AWS CLI installation
+pip install awscli --force-reinstall --upgrade  # AWS CLI installation
 npm install
 npm run deploy
 ls
-echo $AWS_SECRET_ACCESS_KEY
 aws s3 cp node-chat-app.zip $AWS_S3_LOCATION # Uploads the zipfile to S3 and expects the AWS Code Pipeline/Code Deploy to pick up
