2cdf77f7d9d849d88d711e036dd1eaaf65c5b3a2
Adarsh Kumar <clydoskope@gmail.com>
2018-11-07 08:01:50 +0000

    Update deploy.sh

--- deploy/aws-code-deploy/deploy.sh
+++ deploy/aws-code-deploy/deploy.sh
@@ -8,4 +8,7 @@ pip install awscli --force-reinstall --upgrade  # AWS CLI installation
 npm install
 npm run deploy
 ls
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set default.region ${AWS_DEFAULT_REGION}
 aws s3 cp node-chat-app.zip $AWS_S3_LOCATION # Uploads the zipfile to S3 and expects the AWS Code Pipeline/Code Deploy to pick up
