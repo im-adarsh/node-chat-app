6aff022b1c9bda51cdf387840bfbec744d56cf78
Adarsh Kumar <clydoskope@gmail.com>
2018-11-07 08:13:19 +0000

    Update deploy.sh

--- deploy/aws-code-deploy/deploy.sh
+++ deploy/aws-code-deploy/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-apt-get update
+apt-get update && apt-get -y install sudo
 apt-get --quiet install --yes nodejs
 apt-get  --quiet install --yes  npm
 apt-get  --quiet install --yes  python-pip # AWS CLI requires python-pip, python is installed by default
-python3 -m pip uninstall pip && yum apt install python3-pip --reinstall  # pip update 
-yum pip install awscli --force-reinstall --upgrade  # AWS CLI installation
+python3 -m pip uninstall pip && sudo apt install python3-pip --reinstall  # pip update 
+pip install awscli --force-reinstall --upgrade  # AWS CLI installation
 npm install
 npm run deploy
 ls
